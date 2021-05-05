4834f7acddcf45b8bc5be1823b2f1764d9d962be
Carmine Zaccagnino <carmine@carminezacc.com>
2021-05-05 16:08:11 +0200

    Miglioramenti pagina About

--- app/src/main/java/com/carminezacc/morra/About.java
+++ app/src/main/java/com/carminezacc/morra/About.java
@@ -2,11 +2,15 @@ package com.carminezacc.morra;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +64,20 @@ public class About extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button goBackHomeButton = view.findViewById(R.id.aboutToHome);
+
+        goBackHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment.findNavController(About.this)
+                        .navigate(R.id.about_to_home);
+            }
+        });
     }
 }
