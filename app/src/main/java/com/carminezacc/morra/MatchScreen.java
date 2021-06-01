5e3ff6a67648e53b66a96faed07f20de0f175736
Grimos10 <biagiogrimos@gmail.com>
2021-06-01 11:02:27 +0200

    Finita schermata match_screen

--- app/src/main/java/com/carminezacc/morra/MatchScreen.java
+++ app/src/main/java/com/carminezacc/morra/MatchScreen.java
@@ -4,22 +4,40 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class MatchScreen extends Fragment {
+
+    private TextView textView;
+    public NumberPicker numberPicker;
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.records, container, false);
+        return inflater.inflate(R.layout.match_screen, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        textView = view.findViewById(R.id.textViewNumberPicker);
+        numberPicker = view.findViewById(R.id.numberPicker);
+
+        numberPicker.setMaxValue(10);
+        numberPicker.setMinValue(0);
+
+        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                textView.setText("HAI SCELTO DI GRIDARE: " + newVal);
+            }
+        });
     }
 }
