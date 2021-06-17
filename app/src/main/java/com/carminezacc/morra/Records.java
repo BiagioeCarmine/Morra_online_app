9bde6ee2cbe1255465a313c398a3dff25834f067
Grimos10 <biagiogrimos@gmail.com>
2021-06-17 14:34:24 +0200

    Dato un senso alla classe Records (da sistemare la grafica)

--- app/src/main/java/com/carminezacc/morra/Records.java
+++ app/src/main/java/com/carminezacc/morra/Records.java
@@ -1,15 +1,30 @@
 package com.carminezacc.morra;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-//TODO: implementare classe Records, ancora vuota perchè tra poco c'è lezione
+import com.carminezacc.morra.backend.Users;
+import com.carminezacc.morra.interfaces.GetUserHandler;
+import com.carminezacc.morra.models.User;
+import com.carminezacc.morra.state.SessionSingleton;
+
+import java.util.Objects;
+
 public class Records extends Fragment {
+
+    TextView textViewId;
+    TextView textViewPunteggio;
+    TextView textViewSconfitte;
+    TextView textViewVittorie;
+    int userId;
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -21,6 +36,23 @@ public class Records extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        textViewId = view.findViewById(R.id.textViewId);
+        textViewPunteggio = view.findViewById(R.id.textViewPunteggio);
+        textViewSconfitte = view.findViewById(R.id.textViewSconfitte);
+        textViewVittorie = view.findViewById(R.id.textViewVittorie);
+        userId = SessionSingleton.getInstance().getUserId();
+
+        Users.getUser(userId, Objects.requireNonNull(Records.this.getContext()).getApplicationContext(), new GetUserHandler() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void resultReturned(User user) {
+                textViewId.setText("Il tuo id e': " + user.getId());
+                textViewPunteggio.setText("Il tuo punteggio e' di: " + user.getPunteggio() + " punti");
+                textViewVittorie.setText("Hai totalizzato " + user.getVittorie() + " vittorie");
+                textViewSconfitte.setText("Hai totalizzato " + user.getSconfitte() + " sconfitte");
+            }
+        });
 
     }
 }
